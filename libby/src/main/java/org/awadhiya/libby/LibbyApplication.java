5fe2e6539a71ae895fc6ed6526d2840d59703cb9
awadhiya <p_awadhiya@yahoo.com>
2021-09-25 19:54:17 -0400

    test

---
+++ libby/src/main/java/org/awadhiya/libby/LibbyApplication.java
@@ -0,0 +1,13 @@
+package org.awadhiya.libby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibbyApplication.class, args);
+	}
+
+}
---
+++ libby/src/test/java/org/awadhiya/libby/LibbyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.awadhiya.libby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
